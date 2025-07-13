ca8d08910f45dd773b827d8ae772e8b94b12302d
Jalal Hussain <jalal143880@gmail.com>
2025-07-13 13:13:48 +0500

    added routing to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuizPage from "./pages/QuizPage/QuizPage";
 import FormPage from "./pages/FormPage/FormPage";
 import TodoPage from "./pages/TodoPage/TodoPage";
@@ -9,16 +9,6 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      {/* <div className="app-nav">
-        <nav>
-          <ul className="nav-list">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/quiz">Quiz</Link></li>
-            <li><Link to="/form">Form</Link></li>
-            <li><Link to="/todo">Todo</Link></li>
-          </ul>
-        </nav>
-      </div> */}
       <div className="container">
         <Routes>
           <Route path="/" element={<WelcomePage />} />
