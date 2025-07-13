e8771472bf82f82e3a3b8d445c45ae4767aa2c64
Jalal Hussain <jalal143880@gmail.com>
2025-07-13 13:12:11 +0500

    added routing to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-// import TodoPage from "./pages/TodoPage/TodoPage";
-// import FormPage from "./pages/FormPage/FormPage";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import QuizPage from "./pages/QuizPage/QuizPage";
+import FormPage from "./pages/FormPage/FormPage";
+import TodoPage from "./pages/TodoPage/TodoPage";
+import WelcomePage from "./pages/WelcomePage";
+import "./App.css";
+
 function App() {
   return (
-    <div className="container">
-      {/* <TodoPage /> */}
-      {/* <FormPage /> */}
-      <QuizPage />
-    </div>
+    <Router>
+      {/* <div className="app-nav">
+        <nav>
+          <ul className="nav-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/quiz">Quiz</Link></li>
+            <li><Link to="/form">Form</Link></li>
+            <li><Link to="/todo">Todo</Link></li>
+          </ul>
+        </nav>
+      </div> */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/todo" element={<TodoPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
